af03cbcb58a009370ed0c18d3baad76a6f05ff91
David Meister <thedavidmeister@gmail.com>
2023-03-16 10:42:06 +0000

    test

--- test/LibBytes.t.sol
+++ test/LibBytes.t.sol
@@ -7,9 +7,27 @@ import "../src/LibBytes.sol";
 contract LibBytesTest is Test {
     using LibBytes for bytes;
 
-    function testCopyFuzz(bytes memory source_) public {
+    function testCopyFuzz(bytes memory source_, uint256 suffix_) public {
         bytes memory target_ = new bytes(source_.length);
+        uint256 end_;
+        assembly {
+            end_ := add(add(target_, 0x20), mload(target_))
+            mstore(end_, suffix_)
+        }
         LibBytes.unsafeCopyBytesTo(source_.cursor(), target_.cursor(), source_.length);
         assertEq(source_, target_);
+        uint256 suffixAfter_;
+        assembly {
+            suffixAfter_ := mload(end_)
+        }
+        assertEq(suffix_, suffixAfter_);
+    }
+
+    function testCopyMaxSuffixFuzz(bytes memory source_) public {
+        testCopyFuzz(source_, type(uint256).max);
+    }
+
+    function testCopySimple() public {
+        testCopyFuzz(hex"010203", type(uint256).max);
     }
 }
