6ca2f204d92a955b2cc605005c90743c38c3eaa7
David Meister <thedavidmeister@gmail.com>
2023-03-16 11:34:25 +0000

    lint

--- src/LibBytes.sol
+++ src/LibBytes.sol
@@ -3,7 +3,23 @@ pragma solidity ^0.8.16;
 
 type Cursor is uint256;
 
+/// @title LibBytes
+/// @notice Things we want to do carefully and efficiently with `bytes` in memory
+/// that Solidity doesn't give us native tools for.
 library LibBytes {
+    /// Copy an arbitrary number of bytes from one location in memory to another.
+    /// As we can only read/write bytes in 32 byte chunks we first have to loop
+    /// over 32 byte values to copy then handle any unaligned remaining data. The
+    /// remaining data will be appropriately masked with the existing data in the
+    /// final chunk so as to not write past the desired length. Note that the
+    /// final unaligned write will be more gas intensive than the prior aligned
+    /// writes. The writes are completely unsafe, the caller MUST ensure that
+    /// sufficient memory is allocated and reading/writing the requested number
+    /// of bytes from/to the requested locations WILL NOT corrupt memory in the
+    /// opinion of solidity or other subsequent read/write operations.
+    /// @param inputCursor_ The starting location in memory to read from.
+    /// @param outputCursor_ The starting location in memory to write to.
+    /// @param remaining_ The number of bytes to read/write.
     function unsafeCopyBytesTo(Cursor inputCursor_, Cursor outputCursor_, uint256 remaining_) internal pure {
         assembly ("memory-safe") {
             for {} iszero(lt(remaining_, 0x20)) {
@@ -28,6 +44,8 @@ library LibBytes {
         }
     }
 
+    /// Convert a bytes array to a cursor that can be written directly to.
+    /// This is the memory position immediately following the array length.
     function cursor(bytes memory data_) internal pure returns (Cursor cursor_) {
         assembly ("memory-safe") {
             cursor_ := add(data_, 0x20)
