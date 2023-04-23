a290b5a9781b4b33193068ccc52a273c0f53a41a
thedavidmeister <thedavidmeister@gmail.com>
2023-04-23 20:58:02 +0400

    tests

--- src/LibStackSentinel.sol
+++ src/LibStackSentinel.sol
@@ -14,60 +14,18 @@ type Sentinel is uint256;
 library LibStackSentinel {
     using LibStackSentinel for Pointer;
 
-    function consumeSentinelTuple2(Pointer lower, Pointer upper, Sentinel sentinel)
-        internal
-        pure
-        returns (Pointer sentinelPointer, uint256[2][] memory tuples)
-    {
-        unchecked {
-            // Can't consume memory reserved by Solidity. Prevents underflow.
-            if (Pointer.unwrap(lower) < 0x80) revert ReservedPointer(lower);
-            // Upper must not be less than lower.
-            if (Pointer.unwrap(upper) < Pointer.unwrap(lower)) revert InitialStateUnderflow(lower, upper);
-
-            assembly ("memory-safe") {
-                tuples := mload(0x40)
-                for {
-                    let cursor := upper
-                    let tuplesCursor := add(tuples, 0x20)
-                } gt(cursor, lower) {
-                    cursor := sub(cursor, 0x40)
-                    // Write the reference to the tuple
-                    mstore(tuplesCursor, cursor)
-                    tuplesCursor := add(tuplesCursor, 0x20)
-                } {
-                    let potentialSentinelPointer := sub(cursor, 0x20)
-                    if eq(mload(potentialSentinelPointer), sentinel) {
-                        sentinelPointer := potentialSentinelPointer
-                        // Store tuples length.
-                        mstore(tuples, sub(div(sub(tuplesCursor, tuples), 0x20), 1))
-                        // Update allocated memory pointer past the tuples.
-                        mstore(0x40, tuplesCursor)
-                        break
-                    }
-                }
-            }
-
-            if (Pointer.unwrap(sentinelPointer) == 0) {
-                revert MissingSentinel(sentinel);
-            }
-        }
-    }
-
-    /// Given two stack pointers that bound a stack build an array of all values
-    /// above the given sentinel value. The sentinel will be _replaced_ by the
-    /// length of the array, allowing for efficient construction of a valid
-    /// `uint256[]` without additional allocation or copying in memory. As the
-    /// returned value is a `uint256[]` it can be treated as a substack and the
-    /// same (or different) sentinel can be consumed many times to build many
-    /// arrays from the main stack.
+    /// Given two stack pointers that bound a stack build an array of 2 item
+    /// tuples above the given sentinel value. The sentinel will be skipped and
+    /// a pointer below it returned alongside the tuples list.
     ///
-    /// As the sentinel is mutated in place into a length it is NOT safe to call
-    /// this in a context where the stack is expected to be immutable.
+    /// The tuples can be cast (via assembly) to 2-field structs.
+    ///
+    /// The caller MUST consider the region of memory consumed for the structs as
+    /// mutated/truncated/deallocated and reallocated insitu to the tuples.
     ///
     /// The sentinel MUST be chosen to have a negligible chance of colliding with
     /// a real value in the array, otherwise an intended array item will be
-    /// interpreted as a sentinel and the array will be split into two slices.
+    /// interpreted as a sentinel.
     ///
     /// If the sentinel is absent in the stack this WILL REVERT. The intent is
     /// to represent dynamic length arrays without forcing expression authors to
@@ -80,89 +38,47 @@ library LibStackSentinel {
     /// @param sentinel The value to expect as the sentinel. MUST be present in
     /// the stack or `consumeSentinel` will revert. MUST NOT collide with valid
     /// stack items (or be cryptographically improbable to do so).
-    /// @param stepSize Number of items to move over in the array per loop
-    /// iteration. If the array has a known multiple of items it can be more
-    /// efficient to find a sentinel moving in N-item increments rather than
-    /// reading every item individually.
-    function consumeSentinel(Pointer upper, Pointer lower, uint256 sentinel, uint256 stepSize)
+    function consumeSentinelTuple2(Pointer lower, Pointer upper, Sentinel sentinel)
         internal
         pure
-        returns (Pointer, uint256[] memory)
+        returns (Pointer sentinelPointer, uint256[2][] memory tuples)
     {
-        // Disallowed or we hit infinite loop.
-        if (stepSize == 0) revert ZeroStepSize();
-        uint256[] memory array;
-        assembly ("memory-safe") {
-            // Underflow is not allowed and pointing at position 0 in memory is
-            // corrupt behaviour anyway.
-            if iszero(lower) { revert(0, 0) }
-            let sentinelLocation := 0
-            let length := 0
-            let step := mul(stepSize, 0x20)
-            for {
-                upper := sub(upper, 0x20)
-                let end := sub(lower, 0x20)
-            } gt(upper, end) {
-                upper := sub(upper, step)
-                length := add(length, stepSize)
-            } {
-                if eq(sentinel, mload(upper)) {
-                    sentinelLocation := upper
-                    break
-                }
-            }
-            // Sentinel MUST exist in the stack if consumer expects it to there.
-            if iszero(sentinelLocation) { revert(0, 0) }
-            mstore(sentinelLocation, length)
-            array := sentinelLocation
-        }
-        return (upper, array);
-    }
+        // Can't consume memory reserved by Solidity. Prevents underflow.
+        if (Pointer.unwrap(lower) < 0x80) revert ReservedPointer(lower);
+        // Upper must not be less than lower.
+        if (Pointer.unwrap(upper) < Pointer.unwrap(lower)) revert InitialStateUnderflow(lower, upper);
 
-    /// Abstraction over `consumeSentinel` to build an array of solidity structs.
-    /// Solidity won't exactly allow this due to its type system not supporting
-    /// generics, so instead we return an array of references to struct data that
-    /// can be assigned/cast to an array of structs easily with assembly. This
-    /// is NOT intended to be a general purpose workhorse for this task, only
-    /// structs of pointers to `uint256[]` values are supported.
-    ///
-    /// ```
-    /// struct Foo {
-    ///   uint256[] a;
-    ///   uint256[] b;
-    /// }
-    ///
-    /// (StackPointer stackPointer_, uint256[] memory refs_) = consumeStructs(...);
-    /// Foo[] memory foo_;
-    /// assembly ("memory-safe") {
-    ///   mstore(foo_, refs_)
-    /// }
-    /// ```
-    ///
-    /// @param stackTop The top of the stack as per `consumeSentinel`.
-    /// @param stackBottom The bottom of the stack as per `consumeSentinel`.
-    /// @param sentinel The sentinel as per `consumeSentinel`.
-    /// @param structSize The number of `uint256[]` fields on the struct.
-    function consumeStructs(Pointer stackTop, Pointer stackBottom, uint256 sentinel, uint256 structSize)
-        internal
-        pure
-        returns (Pointer, uint256[] memory)
-    {
-        (Pointer stackTopAfter, uint256[] memory tempArray) =
-            stackTop.consumeSentinel(stackBottom, sentinel, structSize);
-        uint256 structsLength = tempArray.length / structSize;
-        uint256[] memory refs = new uint256[](structsLength);
+        // First pass to find the sentinel.
         assembly ("memory-safe") {
-            for {
-                let refCursor := add(refs, 0x20)
-                let refEnd := add(refCursor, mul(mload(refs), 0x20))
-                let tempCursor := add(tempArray, 0x20)
-                let tempStepSize := mul(structSize, 0x20)
-            } lt(refCursor, refEnd) {
-                refCursor := add(refCursor, 0x20)
-                tempCursor := add(tempCursor, tempStepSize)
-            } { mstore(refCursor, tempCursor) }
+            for { let cursor := upper } gt(cursor, lower) { cursor := sub(cursor, 0x40) } {
+                let potentialSentinelPointer := sub(cursor, 0x20)
+                if eq(mload(potentialSentinelPointer), sentinel) { sentinelPointer := potentialSentinelPointer }
+            }
         }
-        return (stackTopAfter, refs);
+
+        if (Pointer.unwrap(sentinelPointer) == 0) {
+            revert MissingSentinel(sentinel);
+        }
+
+        // Second pass to build references _in order_ from the sentinel back up
+        // to upper.
+        assembly ("memory-safe") {
+            tuples := mload(0x40)
+            let tuplesCursor := add(tuples, 0x20)
+            for {
+                let cursor := add(sentinelPointer, 0x20)
+                let end := upper
+            } lt(cursor, end) {
+                tuplesCursor := add(tuplesCursor, 0x20)
+                cursor := add(cursor, 0x40)
+            } {
+                // Write the reference to the tuple
+                mstore(tuplesCursor, cursor)
+            }
+            // Update allocated memory pointer past the tuples.
+            mstore(0x40, tuplesCursor)
+            // Store tuples length.
+            mstore(tuples, sub(div(sub(tuplesCursor, tuples), 0x20), 1))
+        }
     }
 }
--- test/LibStackSentinel.t.sol
+++ test/LibStackSentinel.t.sol
@@ -16,6 +16,7 @@ contract LibStackSentinelTest is Test {
             vm.assume(stack[i] != Sentinel.unwrap(sentinel));
         }
         vm.assume(sentinelIndex < stack.length);
+        // Align the sentinels with clean tuples.
         vm.assume((stack.length - (sentinelIndex + 1)) % 2 == 0);
         stack[sentinelIndex] = Sentinel.unwrap(sentinel);
 
@@ -34,38 +35,134 @@ contract LibStackSentinelTest is Test {
             assertEq(stack[i + 1], tuples[j][1]);
             j++;
         }
+        assertEq(tuples.length * 2, stack.length - (sentinelIndex + 1));
     }
 
-    // function testConsumeSentinelOneSentinel(uint256[] memory stack, uint256 sentinel, uint32 stepSize) public {
-    //     vm.assume(stepSize > 0);
-    //     vm.assume(stepSize < stack.length);
-    //     for (uint256 i = 0; i < stack.length; i++) {
-    //         vm.assume(stack[i] != sentinel);
-    //     }
-    //     stack[stack.length - stepSize - 1] = sentinel;
+    function testConsumeSentinelTuple2MissingSentinel(uint256[] memory stack, Sentinel sentinel) public {
+        for (uint256 i = 0; i < stack.length; i++) {
+            vm.assume(stack[i] != Sentinel.unwrap(sentinel));
+        }
 
-    //     uint256[] memory expectedConsumed = new uint256[](stepSize);
-    //     uint256 j = 0;
-    //     for (uint256 i = stack.length - stepSize; i < stack.length; i++) {
-    //         expectedConsumed[j] = stack[i];
-    //         j++;
-    //     }
+        vm.expectRevert(abi.encodeWithSelector(MissingSentinel.selector, sentinel));
+        (Pointer sentinelPointer, uint256[2][] memory tuples) =
+            stack.dataPointer().consumeSentinelTuple2(stack.endPointer(), sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
 
-    //     (Pointer sentinelPointer, uint256[] memory consumed) =
-    //         stack.endPointer().consumeSentinel(stack.dataPointer(), sentinel, stepSize);
+    function testConsumeSentinelTuple2OddSentinel(uint256[] memory stack, Sentinel sentinel, uint8 sentinelIndex)
+        public
+    {
+        for (uint256 i = 0; i < stack.length; i++) {
+            vm.assume(stack[i] != Sentinel.unwrap(sentinel));
+        }
+        vm.assume(sentinelIndex < stack.length);
+        // UNalign the sentinel with clean tuples.
+        vm.assume((stack.length - (sentinelIndex + 1)) % 2 == 1);
 
-    //     // Sentinel is replaced with length.
-    //     assertEq(Pointer.unwrap(sentinelPointer), Pointer.unwrap(consumed.startPointer()));
-    //     assertEq(sentinelPointer.unsafeReadWord(), stepSize);
-    //     assertEq(expectedConsumed, consumed);
-    // }
+        stack[sentinelIndex] = Sentinel.unwrap(sentinel);
 
-    // function testConsumeSentinelZeroStep(uint256[] memory stack, uint256 sentinel, bytes memory seed) public {
-    //     if (stack.length > 0) {
-    //         stack[uint256(keccak256(seed)) % stack.length] = sentinel;
-    //     }
-    //     vm.expectRevert(ZeroStepSize.selector);
-    //     (Pointer sentinelPointer, uint256[] memory consumed) =
-    //         stack.endPointer().consumeSentinel(stack.dataPointer(), sentinel, 0);
-    // }
+        vm.expectRevert(abi.encodeWithSelector(MissingSentinel.selector, sentinel));
+        (Pointer sentinelPointer, uint256[2][] memory tuples) =
+            stack.dataPointer().consumeSentinelTuple2(stack.endPointer(), sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
+
+    function testConsumeSentinelTuple2ReservedPointerError(Pointer lower, Pointer upper, Sentinel sentinel) public {
+        vm.assume(Pointer.unwrap(lower) < 0x80);
+
+        vm.expectRevert(abi.encodeWithSelector(ReservedPointer.selector, lower));
+        (Pointer sentinelPointer, uint256[2][] memory tuples) = lower.consumeSentinelTuple2(upper, sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
+
+    function testConsumeSentinelTuple2InitialStateUnderflowError(Pointer lower, Pointer upper, Sentinel sentinel)
+        public
+    {
+        vm.assume(Pointer.unwrap(lower) >= 0x80);
+        vm.assume(Pointer.unwrap(upper) < Pointer.unwrap(lower));
+
+        vm.expectRevert(abi.encodeWithSelector(InitialStateUnderflow.selector, lower, upper));
+        (Pointer sentinelPointer, uint256[2][] memory tuples) = lower.consumeSentinelTuple2(upper, sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
+
+    function testConsumeSentinelTuple2Empty(Sentinel sentinel) public {
+        Pointer lower;
+        assembly ("memory-safe") {
+            lower := mload(0x40)
+            mstore(lower, sentinel)
+        }
+
+        vm.expectRevert(abi.encodeWithSelector(MissingSentinel.selector, sentinel));
+        (Pointer sentinelPointer0, uint256[2][] memory tuples0) = lower.consumeSentinelTuple2(lower, sentinel);
+        (sentinelPointer0);
+        (tuples0);
+
+        (Pointer sentinelPointer1, uint256[2][] memory tuples1) =
+            lower.consumeSentinelTuple2(lower.unsafeAddWord(), sentinel);
+        (sentinelPointer1);
+        (tuples1);
+        assertEq(Pointer.unwrap(sentinelPointer1), Pointer.unwrap(lower));
+        assertEq(tuples1.length, 0);
+    }
+
+    function testConsumeSentinelTuple2Gas0() public pure {
+        Pointer lower;
+        Pointer upper;
+        Sentinel sentinel = Sentinel.wrap(50);
+        assembly ("memory-safe") {
+            lower := mload(0x40)
+            upper := add(lower, 0x20)
+            mstore(lower, sentinel)
+        }
+        (Pointer sentinelPointer, uint256[2][] memory tuples) = lower.consumeSentinelTuple2(upper, sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
+
+    function testConsumeSentinelTuple2Gas1() public pure {
+        Pointer lower;
+        Pointer upper;
+        Sentinel sentinel = Sentinel.wrap(50);
+        assembly ("memory-safe") {
+            lower := mload(0x40)
+            upper := add(lower, 0x60)
+            mstore(lower, sentinel)
+        }
+        (Pointer sentinelPointer, uint256[2][] memory tuples) = lower.consumeSentinelTuple2(upper, sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
+
+    function testConsumeSentinelTuple2Gas2() public pure {
+        Pointer lower;
+        Pointer upper;
+        Sentinel sentinel = Sentinel.wrap(50);
+        assembly ("memory-safe") {
+            lower := mload(0x40)
+            upper := add(lower, 0xa0)
+            mstore(lower, sentinel)
+        }
+        (Pointer sentinelPointer, uint256[2][] memory tuples) = lower.consumeSentinelTuple2(upper, sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
+
+    function testConsumeSentinelTuple2Gas3() public pure {
+        Pointer lower;
+        Pointer upper;
+        Sentinel sentinel = Sentinel.wrap(50);
+        assembly ("memory-safe") {
+            lower := mload(0x40)
+            upper := add(lower, 0xe0)
+            mstore(lower, sentinel)
+        }
+        (Pointer sentinelPointer, uint256[2][] memory tuples) = lower.consumeSentinelTuple2(upper, sentinel);
+        (sentinelPointer);
+        (tuples);
+    }
 }
